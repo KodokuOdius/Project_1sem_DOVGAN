5862ee6bcfc91dacc1c025c55100e1609c58c152
KodokuOdius <80702757+KodokuOdius@users.noreply.github.com>
2021-11-14 16:01:53 +0300

    Add files via upload

---
+++ PracticalWork5/exercises/__init__.py
@@ -0,0 +1,1 @@
+from exercises import variant_6
---
+++ PracticalWork5/main5.py
@@ -0,0 +1,13 @@
+from exercises import *
+
+while True:
+    var = input("Введите номер (0 - выход) варианта: ")
+    if var == "0":
+        print("Выход из программы\n")
+        break
+    
+    elif var == "6":
+        variant_6.solve()   
+
+    else:
+        print("Нет такого варианта")

3fbfe37f7dff9460aada358b5eeeb258d6eaa30f
KodokuOdius <alanfoxincoolctranica@gmail.com>
2022-01-13 12:38:23 +0300

    sceleton

--- PracticalWork12/main.py
+++ PracticalWork12/main.py
@@ -20,10 +20,135 @@ from tkinter import *
 
 root = Tk()
 root.title("Registration form")
+root.rowconfigure(
+    [i for i in range(9)],
+    weight=10,
+    minsize=25
+)
+root.columnconfigure(
+    [i for i in range(2)],
+    weight=10,
+    minsize=100
+)
 root.resizable(
     width=False,
     height=False
 )
 
 
-root.mainloop()
+main_lbl = Label(
+    master=root,
+    text="EVENT REGISTRATION FORM",
+    bg="black",
+    fg="white"
+)
+main_lbl.grid(
+    row=0,
+    column=0,
+    columnspan=2,
+    sticky="swen"
+)
+
+
+Label(master=root, text="Name").grid(row=1, column=0, sticky="swen")
+frm_enter_name = Frame(master=root)
+frm_enter_name.rowconfigure(
+    [0, 1],
+    weight=1,
+    minsize=25
+)
+frm_enter_name.columnconfigure(
+    [0, 1],
+    weight=1,
+    minsize=100
+)
+frm_enter_name.grid(row=1, column=1, sticky="swen")
+
+frt_name = Entry(master=frm_enter_name)
+frt_name.grid(row=0, column=0, sticky="swen")
+Label(master=frm_enter_name, text="First Name").grid(row=1, column=0, sticky="nw")
+
+snd_name = Entry(master=frm_enter_name)
+snd_name.grid(row=0, column=1, sticky="swen")
+Label(master=frm_enter_name, text="Second Name").grid(row=1, column=1, sticky="nw")
+
+
+Label(master=root, text="Company").grid(row=2, column=0, sticky="swen")
+copmany_ent = Entry(master=root)
+copmany_ent.grid(row=2, column=1, sticky="swen")
+
+
+Label(master=root, text="Email").grid(row=3, column=0, sticky="swen")
+email_ent = Entry(master=root)
+email_ent.grid(row=3, column=1, sticky="swen")
+
+
+Label(master=root, text="Phone").grid(row=4, column=0, sticky="swen")
+frm_enter_phone = Frame(master=root)
+frm_enter_phone.rowconfigure(
+    [0, 1],
+    weight=1,
+    minsize=25
+)
+frm_enter_phone.columnconfigure(
+    [0, 1, 2, 3],
+    weight=1,
+    minsize=100
+)
+frm_enter_phone.grid(row=4, column=1, sticky="swen")
+
+area_code = Entry(master=frm_enter_phone)
+area_code.grid(row=0, column=0, sticky="swen")
+Label(master=frm_enter_phone, text="Area Code").grid(row=1, column=0, sticky="swen")
+
+phone_num = Entry(master=frm_enter_phone)
+phone_num.grid(row=0, column=1, columnspan=3, sticky="swen")
+Label(master=frm_enter_phone, text="Phone Number").grid(row=1, column=1, sticky="swen")
+
+
+Label(master=root, text="Subject").grid(row=5, column=0, sticky="swen")
+subj_lst = [f"Subj{i+1}" for i in range(4)]
+variable = StringVar(root)
+variable.set("")
+
+opt_subj = OptionMenu(root, variable, *subj_lst)
+opt_subj.grid(row=5, column=1, sticky="swen")
+
+
+frm_rad = Frame(master=root)
+frm_rad.rowconfigure(
+    [0, 1],
+    weight=1,
+    minsize=25
+)
+frm_rad.columnconfigure(
+    [0, 1],
+    weight=1,
+    minsize=100
+)
+frm_rad.grid(row=7, column=0, sticky="swen")
+
+Label(master=frm_rad, text="Question").grid(row=0, column=0, columnspan=2, sticky="swen")
+
+rad_y = Radiobutton(master=frm_rad, text="Yes", variable=1)
+rad_y.grid(row=1, column=0, sticky="swen")
+
+rad_n = Radiobutton(master=frm_rad, text="No", variable=1)
+rad_n.grid(row=1 ,column=1, sticky="swen")
+
+"""
+reg_btn = Button(
+    master=frm_rad,
+    text="Registration"
+)
+reg_btn.grid(row=2, column=0, columnspan=2, sticky="swen")
+"""
+
+reg_btn = Button(
+    master=root,
+    text="Registration"
+)
+reg_btn.grid(row=8, column=0, sticky="swen")
+
+if __name__ == "__main__":
+    root.mainloop()

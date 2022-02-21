58f17bd44cc180d25b2d444c34e72dc85734f8d0
KodokuOdius <alanfoxincoolctranica@gmail.com>
2022-02-21 20:01:52 +0300

    start 14

---
+++ PracticalWork14/variant_6.py
@@ -0,0 +1,13 @@
+from exersices import ex1
+
+
+print(
+    "Приветствую тебя в Практическом задании номер 6"
+)
+while True:
+    ex = input("Ввeди номер задания\n Зотя он тут один = D :")
+    if ex == "1":
+        ex1.solve()
+        
+    else:
+        print("Не угадал))")
